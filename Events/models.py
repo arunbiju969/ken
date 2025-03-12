3b0e963acc27e9daa5abdd2fa0040f0203bac36f
BubuSuhas <bubusuhas@gmail.com>
2025-03-12 13:58:39 -0300

    model_event

--- Events/models.py
+++ Events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    event_type = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    registration_deadline = models.DateTimeField()
+    image_url = models.URLField(max_length=200)
+    event_url = models.URLField(max_length=200)
+    event_rules = models.TextField()
+
+    def __str__(self):
+        return self.title
